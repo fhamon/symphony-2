9b3fffb69c5eb1861490c7d40dcc8a89258f0144
Alexandra Nantel <alexandra@deuxhuithuit.com>
2018-06-29 14:25:36 -0400

    Admin - Filter Icon
    Add Filter icon to action button.
    Rework inverted logic of insertAction.

--- symphony/lib/toolkit/class.administrationpage.php
+++ symphony/lib/toolkit/class.administrationpage.php
@@ -256,9 +256,9 @@ class AdministrationPage extends HTMLPage
         $li = new XMLElement('li', $action);
 
         if ($append) {
-            $ul->prependChild($li);
-        } else {
             $ul->appendChild($li);
+        } else {
+            $ul->prependChild($li);
         }
     }
 
@@ -316,7 +316,7 @@ class AdministrationPage extends HTMLPage
      *  If any other value is passed, no button will be added.
      * @throws InvalidArgumentException
      */
-    public function insertDrawer(XMLElement $drawer, $position = 'horizontal', $button = 'append')
+    public function insertDrawer(XMLElement $drawer, $position = 'horizontal', $button = 'append', $icon = '')
     {
         $drawer->addClass($position);
         $drawer->setAttribute('data-position', $position);
@@ -326,7 +326,7 @@ class AdministrationPage extends HTMLPage
         if (in_array($button, array('prepend', 'append'))) {
             $this->insertAction(
                 Widget::Anchor(
-                    $drawer->getAttribute('data-label'),
+                    ($icon === '') ? $drawer->getAttribute('data-label') : $icon . '<span><span>' . $drawer->getAttribute('data-label') . '</span></span>',
                     '#' . $drawer->getAttribute('id'),
                     null,
                     'button drawer ' . $position
