a39ac84d0232ec11f68722556248b5b4a6d6cc8b
Frederick Hamon <f.hamon73@gmail.com>
2018-12-07 10:35:44 -0500

    Added index link in toolbar

--- symphony/lib/toolkit/class.administrationpage.php
+++ symphony/lib/toolkit/class.administrationpage.php
@@ -507,6 +507,11 @@ class AdministrationPage extends HTMLPage
             )
         );
 
+        $this->Tools->appendChild(Widget::Anchor(
+            'index',
+            rtrim(URL, '/') . '/'
+        ));
+
         $this->appendUserLinks();
         $this->appendNavigation();
 
